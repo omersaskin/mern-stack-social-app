024771f1357b3e79ec42cbfe377899fef354d2fd
omersaskin <pc.omer.saskin@gmail.com>
2023-09-07 10:42:46 +0300

    Router eklendi, Hoisting yapıldı, .env eklendi
    npm install react-router-dom

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
