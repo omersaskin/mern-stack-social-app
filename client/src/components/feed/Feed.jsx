be54a95095e9f0dfdc945963d3160f7dc238a2c2
omersaskin <pc.omer.saskin@gmail.com>
2023-09-07 11:40:31 +0300

    Fetching data with Axios and Hooks eklendi
    npm install axios

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,14 +1,28 @@
+import { useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
 import { Posts } from "../../dummyData";
+import axios from "axios";
 
 export default function Feed() {
+  const [posts, setPosts] = useState([])
+  const [text, setText] = useState("")
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("posts/timeline/64f84c10380f1599e8d536e9")
+      setPosts(res.data)
+    }
+    
+    fetchPosts();
+  })
+
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map((p) => (
+        {posts.map((p) => (
           <Post key={p.id} post={p} />
         ))}
       </div>
