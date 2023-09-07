5020d0613db4e8add839b2400d2cb9664fdfbe67
omersaskin <pc.omer.saskin@gmail.com>
2023-09-07 12:42:48 +0300

    Fetching user inside useEffect with Axios eklendi
    npm install timeago.js

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -2,12 +2,10 @@ import { useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
-import { Posts } from "../../dummyData";
 import axios from "axios";
 
 export default function Feed() {
   const [posts, setPosts] = useState([])
-  const [text, setText] = useState("")
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -16,14 +14,14 @@ export default function Feed() {
     }
     
     fetchPosts();
-  })
+  }, [])
 
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
         {posts.map((p) => (
-          <Post key={p.id} post={p} />
+          <Post key={p._id} post={p} />
         ))}
       </div>
     </div>
